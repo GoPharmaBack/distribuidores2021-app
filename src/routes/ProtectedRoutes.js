a7efbd25766cbf4948391b8bd2a36c6d74741204
Carlos Boyzo <carlosboyzo@MacBook-Pro-de-Carlos.local>
2021-09-06 02:24:10 -0500

    fix varable

--- src/routes/ProtectedRoutes.js
+++ src/routes/ProtectedRoutes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Route,Redirect} from "react-router-dom"
 
-function ProtectedRoutes({isAuth: isAuth, component: Component,...rest}) {
+function ProtectedRoutes({isAuth0: isAuth, component: Component,...rest}) {
   return (
     <Route {...rest} 
     render={(props) =>{
