1d5a5191d7a3e93c825af4028ec424a34cc1bbad
Carlos Boyzo <carlosboyzo@MacBook-Pro-de-Carlos.local>
2021-09-06 02:18:51 -0500

    set

--- src/views/Error.js
+++ src/views/Error.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Error() {
   return (
     <section>
       <h1>Ups 😢</h1>
+      <Link to="/">regresa al inicio</Link>
     </section>
   );
 }
