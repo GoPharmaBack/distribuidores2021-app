80dc4deb2820ef42edef039974929421e17c17c2
Carlos Boyzo <carlosboyzo@iMac-de-Carlos.local>
2021-09-29 00:44:57 -0500

    FIX BUG Delete variable

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState } from "react";
 import { FormattedMessage } from "react-intl";
 
 const Clock = ({ deadline }) => {
