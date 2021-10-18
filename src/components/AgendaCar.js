5d27f7e67fba89ebb12d6c8eec0d6dd7a0ec0b92
Carlos Boyzo <carlosboyzo@MacBook-Pro-de-Carlos.local>
2021-10-18 11:38:28 -0500

    Ajuste Agenda

--- src/components/AgendaCar.js
+++ src/components/AgendaCar.js
@@ -1,26 +1,21 @@
 import React, { useState, useContext } from "react";
 import { Modal, Button } from "react-bootstrap";
 
-
 import { LangContext } from "../context/LangContext";
 import { FormattedMessage } from "react-intl";
 function AgendaCar() {
-
-
- 
   const idioma = useContext(LangContext);
- 
+
   console.log(idioma);
 
   const [modalShow1, setModalShow1] = useState(false);
   const [modalShow2, setModalShow2] = useState(false);
 
-
   let Agenda19 =
- "https://firebasestorage.googleapis.com/v0/b/distribuidores2021-d71be.appspot.com/o/AGENDA%2FCARIBBEAN_AGENDA%20OCTUBRE_DISTRIBUIDORES_19.png?alt=media&token=c5333a4b-902b-4bc4-a87a-78dcf568bd8f";
+    "https://firebasestorage.googleapis.com/v0/b/baxterhospitalproduct.appspot.com/o/caribbean-shedule%2FPROGRAMACION_CARIBBEAN_AGENDA%20OCTUBRE_DISTRIBUIDORES_19.png?alt=media&token=d55b753d-bb95-47dd-b682-6fc4eed7f258";
 
-let Agenda20 =
-"https://firebasestorage.googleapis.com/v0/b/distribuidores2021-d71be.appspot.com/o/AGENDA%2FCARIBBEAN_AGENDA%20OCTUBRE_DISTRIBUIDORES_20.png?alt=media&token=49a62290-ba1b-4dc1-b2e9-2e51ba1cb293"
+  let Agenda20 =
+    "https://firebasestorage.googleapis.com/v0/b/baxterhospitalproduct.appspot.com/o/caribbean-shedule%2FPROGRAMACION_CARIBBEAN_AGENDA%20OCTUBRE_DISTRIBUIDORES_20.png?alt=media&token=597b7779-b998-4ff1-b5ef-36d675707195";
   function MyVerticallyCenteredModal(props) {
     return (
       <Modal
@@ -39,7 +34,6 @@ let Agenda20 =
       </Modal>
     );
   }
-
 
   function MyVerticallyCenteredModalDos(props) {
     return (
@@ -62,27 +56,31 @@ let Agenda20 =
   return (
     <>
       {" "}
-      <div className="agenda" >
-   
-      <h3 id="agenda"><FormattedMessage id="title.agenda" /> CAR </h3>
-         <div className="contenedor-botones-agenda">
-         <button className="btn-sm btn-primary p-2 m-1" onClick={() => setModalShow1(true)}>
+      <div className="agenda">
+        <h3 id="agenda">
+          <FormattedMessage id="title.agenda" /> CAR{" "}
+        </h3>
+        <div className="contenedor-botones-agenda">
+          <button
+            className="btn-sm btn-primary p-2 m-1"
+            onClick={() => setModalShow1(true)}
+          >
             <FormattedMessage id="diauno.agenda" />
           </button>
 
-          <button className="btn-sm btn-primary p-2 m-1" onClick={() => setModalShow2(true)}>
+          <button
+            className="btn-sm btn-primary p-2 m-1"
+            onClick={() => setModalShow2(true)}
+          >
             <FormattedMessage id="diados.agenda" />
           </button>
-         </div>
-        
+        </div>
       </div>
-      
       <MyVerticallyCenteredModal
         show={modalShow1}
         onHide={() => setModalShow1(false)}
       />
-
-<MyVerticallyCenteredModalDos
+      <MyVerticallyCenteredModalDos
         show={modalShow2}
         onHide={() => setModalShow2(false)}
       />
