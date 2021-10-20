da844ecda7d6198ec6a0abbe0d13755ba464d60a
Carlos Boyzo <carlosboyzo@MacBook-Pro-de-Carlos.local>
2021-10-20 16:15:03 -0500

    ajuste

--- src/views/SalaMx.js
+++ src/views/SalaMx.js
@@ -68,7 +68,7 @@ function SalaMx() {
               <div style={{ padding: "56.25% 0 0 0", position: "relative" }}>
                 <iframe
                   title="SalaMx"
-                  src="https://vimeo.com/event/1384771/embed"
+                  src="https://vimeo.com/637221135/embed"
                   frameBorder={0}
                   allow="autoplay; fullscreen; picture-in-picture"
                   allowFullScreen
